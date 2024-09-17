1b25ebe31acdb75901923dd90c7be7eb3e955fa1
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-09-17 12:57:42 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_REGNAULT/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,21 @@
+/*
+* TP0 Exo1
+* Louis Regnault-Loquet
+ */
+package exo1;
+
+/**
+ *
+ * @author louis
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("Bonjour");
+         
+    }
+    
+}
