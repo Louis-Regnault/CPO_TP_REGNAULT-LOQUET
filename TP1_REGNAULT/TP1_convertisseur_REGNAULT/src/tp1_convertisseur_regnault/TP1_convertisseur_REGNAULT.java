b73ebab4885ea4ff057b7bdfce86a9c93484fe8f
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-09-20 18:28:46 +0200

    Création d'un nouveau projet versionné

--- TP1_REGNAULT/TP1_convertisseur_REGNAULT/src/tp1_convertisseur_regnault/TP1_convertisseur_REGNAULT.java
+++ TP1_REGNAULT/TP1_convertisseur_REGNAULT/src/tp1_convertisseur_regnault/TP1_convertisseur_REGNAULT.java
@@ -18,9 +18,11 @@ public class TP1_convertisseur_REGNAULT {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Veuillez entrer une valeur réelle : ");
-        double valeurReelle = sc.nextDouble();
-        System.out.println("La valeur réelle saisie est : " + valeurReelle);
+        System.out.print("Veuillez entrer une température en degrés Celsius : ");
+        double temperatureCelsius = sc.nextDouble();
+        double temperatureKelvin = temperatureCelsius + 273.15;
+        System.out.println("La température saisie en degrés Celsius est : " + temperatureCelsius);
+        System.out.println("La température correspondante en Kelvin est : " + temperatureKelvin);
     }
     
 }
