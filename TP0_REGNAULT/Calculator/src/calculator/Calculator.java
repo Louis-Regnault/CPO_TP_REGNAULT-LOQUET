c1410727f573b36cc5bee2eb02a109df37e11c3d
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-09-20 17:01:30 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_REGNAULT/Calculator/src/calculator/Calculator.java
@@ -0,0 +1,26 @@
+/*
+*Exercice 3 : Calculator
+*TP0
+*Louis Regnault-Loquet
+ */
+package calculator;
+
+/**
+ *
+ * @author louis
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("""
+                          Please enter the operator :
+                          1)add
+                          2)substract
+                          3)multiply
+                          4)divide
+                          5)modulo"""); 
+    }
+}
