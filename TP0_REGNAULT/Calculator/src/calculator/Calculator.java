071daf3caf8848b1da78c8956ecd96f434a13e5a
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-09-20 17:11:32 +0200

    Création d'un nouveau projet versionné

--- TP0_REGNAULT/Calculator/src/calculator/Calculator.java
+++ TP0_REGNAULT/Calculator/src/calculator/Calculator.java
@@ -5,6 +5,8 @@
  */
 package calculator;
 
+import java.util.Scanner;
+
 /**
  *
  * @author louis
@@ -15,12 +17,16 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int operateur = 0;
+        Scanner sc = new Scanner(System.in);
         System.out.print("""
                           Please enter the operator :
                           1)add
                           2)substract
                           3)multiply
                           4)divide
-                          5)modulo"""); 
+                          5)modulo""");
+        operateur = sc.nextInt();
+        System.out.println("Vous avez choisi l'operateur: " + operateur);
     }
 }
