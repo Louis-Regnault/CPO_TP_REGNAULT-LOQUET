d84f730e8bd5aaa34b84f1c4a1a7177ce6301a3e
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-09-20 17:46:17 +0200

    Création d'un nouveau projet versionné

--- TP0_REGNAULT/Calculator/src/calculator/Calculator.java
+++ TP0_REGNAULT/Calculator/src/calculator/Calculator.java
@@ -18,6 +18,7 @@ public class Calculator {
      */
     public static void main(String[] args) {
         int operateur = 0;
+        double operande1,operande2,resultat=0;
         Scanner sc = new Scanner(System.in);
         System.out.print("""
                           Please enter the operator :
@@ -28,5 +29,11 @@ public class Calculator {
                           5)modulo""");
         operateur = sc.nextInt();
         System.out.println("Vous avez choisi l'operateur: " + operateur);
+        System.out.print("Veuillez entrer la première valeur : ");
+        operande1 = sc.nextDouble();  
+        System.out.println("La première valeur saisie est : " + operande1);
+        System.out.print("Veuillez entrer la deuxième valeur : ");
+        operande2 = sc.nextDouble();  
+        System.out.println("La deuxième valeur saisie est : " + operande2);
     }
 }
