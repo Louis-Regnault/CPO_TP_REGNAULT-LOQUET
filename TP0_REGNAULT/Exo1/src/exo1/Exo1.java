ff9b67e2962cdd26d3f49e77f206239e205d55fb
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-09-20 16:28:33 +0200

    Création d'un nouveau projet versionné

--- TP0_REGNAULT/Exo1/src/exo1/Exo1.java
+++ TP0_REGNAULT/Exo1/src/exo1/Exo1.java
@@ -1,8 +1,12 @@
 /*
 * TP0 Exo1
 * Louis Regnault-Loquet
+* Groupe : TDB
+* Septembre 2024
  */
 package exo1;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +19,12 @@ public class Exo1 {
      */
     public static void main(String[] args) {
          System.out.println("Bonjour");
-         
+         System.out.println("Au revoir");
+         String prenom;
+ Scanner sc;
+ sc = new Scanner(System.in);
+ System.out.println("Bonjour, quel est votre prenom ?");
+ prenom = sc.nextLine();
     }
     
 }
