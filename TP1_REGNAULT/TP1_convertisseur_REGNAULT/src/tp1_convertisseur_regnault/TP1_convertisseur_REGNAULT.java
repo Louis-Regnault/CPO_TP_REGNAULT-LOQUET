f3333ffb143e3a84e8bbbfc77b33c7569c84054b
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-09-23 12:23:58 +0200

    Création d'un nouveau projet versionné

--- TP1_REGNAULT/TP1_convertisseur_REGNAULT/src/tp1_convertisseur_regnault/TP1_convertisseur_REGNAULT.java
+++ TP1_REGNAULT/TP1_convertisseur_REGNAULT/src/tp1_convertisseur_regnault/TP1_convertisseur_REGNAULT.java
@@ -12,7 +12,28 @@ import java.util.Scanner;
  * @author louis
  */
 public class TP1_convertisseur_REGNAULT {
-
+    
+    public static double CelciusVersKelvin(double tCelcius) {
+        return tCelcius + 273.15;
+    }
+    public static double KelvinVersCelcius(double tKelvin) {
+        return tKelvin - 273.15;
+    }
+    public static double FarenheitVersCelcius(double tFahrenheit) {
+        return (tFahrenheit - 32) * 5 / 9;
+    }
+    public static double CelciusVersFarenheit(double tCelcius) {
+        return (tCelcius * 9 / 5) + 32;
+    }
+    public static double KelvinVersFarenheit(double tKelvin) {
+        double tCelcius = KelvinVersCelcius(tKelvin);
+        return CelciusVersFarenheit(tCelcius);
+    }
+    public static double FarenheitVersKelvin(double tFahrenheit) {
+        double tCelcius = FarenheitVersCelcius(tFahrenheit);
+        return CelciusVersKelvin(tCelcius);
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -24,5 +45,6 @@ public class TP1_convertisseur_REGNAULT {
         System.out.println("La température saisie en degrés Celsius est : " + temperatureCelsius);
         System.out.println("La température correspondante en Kelvin est : " + temperatureKelvin);
     }
-    
 }
+
+
