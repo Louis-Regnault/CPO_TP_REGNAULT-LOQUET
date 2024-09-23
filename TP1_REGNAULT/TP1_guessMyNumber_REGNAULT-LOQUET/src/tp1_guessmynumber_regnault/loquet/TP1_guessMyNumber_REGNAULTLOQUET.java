7b7c031fabcf4ea6206139af4051b11c8dafdbfe
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-09-23 12:54:29 +0200

    Création d'un nouveau projet versionné

---
+++ TP1_REGNAULT/TP1_guessMyNumber_REGNAULT-LOQUET/src/tp1_guessmynumber_regnault/loquet/TP1_guessMyNumber_REGNAULTLOQUET.java
@@ -0,0 +1,21 @@
+/*
+ * TP1
+ * guessMyNumber
+ * Louis REGNAULT-LOQUET
+ */
+package tp1_guessmynumber_regnault.loquet;
+
+/**
+ *
+ * @author louis
+ */
+public class TP1_guessMyNumber_REGNAULTLOQUET {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
