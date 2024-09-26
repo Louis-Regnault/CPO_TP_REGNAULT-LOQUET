6e39c33a57e86879017735252a9424f67f57fc03
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-09-26 14:14:49 +0200

    Création d'un nouveau projet versionné

---
+++ TP2_REGNAULT/TP2_Bieres_REGNAULT-LOQUET/src/tp2_bieres_regnault/loquet/BouteilleBiere.java
@@ -0,0 +1,14 @@
+/*
+ * TP2
+ * Exercice BouteilleBiere
+ * Louis REGNAULT-LOQUET
+ */
+package tp2_bieres_regnault.loquet;
+
+/**
+ *
+ * @author louis
+ */
+public class BouteilleBiere {
+    
+}
---
+++ TP2_REGNAULT/TP2_Bieres_REGNAULT-LOQUET/src/tp2_bieres_regnault/loquet/TP2_Bieres_REGNAULTLOQUET.java
@@ -0,0 +1,21 @@
+/*
+ * TP2
+ * Exercice Bieres
+ * Louis REGNAULT-LOQUET
+ */
+package tp2_bieres_regnault.loquet;
+
+/**
+ *
+ * @author louis
+ */
+public class TP2_Bieres_REGNAULTLOQUET {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
