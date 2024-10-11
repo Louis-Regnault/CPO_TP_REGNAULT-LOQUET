ea0c26eb4764fc9e63e88d6e22bdf2b6c922ef9b
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-10-11 08:37:41 +0200

    Création d'un nouveau projet versionné

--- TP2_REGNAULT/TP2_Bieres_REGNAULT-LOQUET/src/tp2_bieres_regnault/loquet/TP2_Bieres_REGNAULTLOQUET.java
+++ TP2_REGNAULT/TP2_Bieres_REGNAULT-LOQUET/src/tp2_bieres_regnault/loquet/TP2_Bieres_REGNAULTLOQUET.java
@@ -17,19 +17,19 @@ public class TP2_Bieres_REGNAULTLOQUET {
     
         public static void main(String[] args) {
         // Utilisation du constructeur
-        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des Trolls", 7.0f, "Dubuisson");
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des Trolls", 7.0, "Dubuisson");
         uneBiere.lireEtiquette();  // Affichage de l'étiquette de la première bière
         
-        BouteilleBiere uneSecondeBiere = new BouteilleBiere("Leffe", 6.6f, "Abbaye de Leffe");
+        BouteilleBiere uneSecondeBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
         uneSecondeBiere.lireEtiquette();
         
-        BouteilleBiere troisiemeBiere = new BouteilleBiere("Chimay Bleue", 9.0f, "Abbaye de Scourmont");
+        BouteilleBiere troisiemeBiere = new BouteilleBiere("Chimay Bleue", 9.0, "Abbaye de Scourmont");
         troisiemeBiere.lireEtiquette();  // Affichage de l'étiquette de la troisième bière
         
-        BouteilleBiere quatriemeBiere = new BouteilleBiere("Kwak", 8.4f, "Brouwerij Bosteels");
+        BouteilleBiere quatriemeBiere = new BouteilleBiere("Kwak", 8.4, "Brouwerij Bosteels");
         quatriemeBiere.lireEtiquette();  // Affichage de l'étiquette de la quatrième bière
         
-        BouteilleBiere cinquiemeBiere = new BouteilleBiere("Hoegaarden", 4.9f, "Brouwerij van Hoegaarden");
+        BouteilleBiere cinquiemeBiere = new BouteilleBiere("Hoegaarden", 4.9, "Brouwerij van Hoegaarden");
         cinquiemeBiere.lireEtiquette();
         
         uneBiere.Decapsuler();       // Décapsule première bière
