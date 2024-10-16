c171da79faad909e6cc9ae68b15296b413ce2992
Louis Regnault-Loquet <louis.regnault-loquet@epfedu.fr>
2024-10-16 11:14:02 +0200

    Création d'un nouveau projet versionné

--- TP2_REGNAULT/TP2_manip_REGNAULT-LOQUET/src/tp2_manip_regnault/loquet/Moussaka.java
+++ TP2_REGNAULT/TP2_manip_REGNAULT-LOQUET/src/tp2_manip_regnault/loquet/Moussaka.java
@@ -13,7 +13,34 @@ public class Moussaka {
 
     public Moussaka(int nbCalories) {
         this.nbCalories = nbCalories;
+    
+    Moussaka[] tabMoussaka = new Moussaka[10];
+    
+        tabMoussaka[0] = new Moussaka(500);
+        tabMoussaka[1] = new Moussaka(600);
+        tabMoussaka[2] = new Moussaka(700);
+        tabMoussaka[3] = new Moussaka(800);
+        tabMoussaka[4] = new Moussaka(900);
+        tabMoussaka[5] = new Moussaka(1000);
+        tabMoussaka[6] = new Moussaka(1100);
+        tabMoussaka[7] = new Moussaka(1200);
+        tabMoussaka[8] = new Moussaka(1300);
+        tabMoussaka[9] = new Moussaka(1400);
+        
+        tabMoussaka[0] = afficherCalories();
+        tabMoussaka[1] = afficherCalories();
+        tabMoussaka[2] = afficherCalories();
+        tabMoussaka[3] = afficherCalories();
+        tabMoussaka[4] = afficherCalories();
+        tabMoussaka[5] = afficherCalories();
+        tabMoussaka[6] = afficherCalories();
+        tabMoussaka[7] = afficherCalories();
+        tabMoussaka[8] = afficherCalories();
+        tabMoussaka[9] = afficherCalories();
     }
-    
-    
+
+    private Moussaka afficherCalories() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
+  
--- TP2_REGNAULT/TP2_manip_REGNAULT-LOQUET/src/tp2_manip_regnault/loquet/TP2_manip_REGNAULTLOQUET.java
+++ TP2_REGNAULT/TP2_manip_REGNAULT-LOQUET/src/tp2_manip_regnault/loquet/TP2_manip_REGNAULTLOQUET.java
@@ -17,11 +17,19 @@ public class TP2_manip_REGNAULTLOQUET {
         Tartiflette assiette1 = new Tartiflette(500);
         Tartiflette assiette2 = new Tartiflette(600);
         Tartiflette assiette3 = assiette2;
+        assiette3.nbCalories += 100;
+        
+        Tartiflette temp = assiette1;  // Sauvegarde calories assiette1
+        assiette1 = assiette2;      
+        assiette2 = temp;
 
         System.out.println("Calories dans assiette 1 : " + assiette1.nbCalories);
-        System.out.println("Calories dans assiette 2 : " + assiette2.nbCalories++);
+        System.out.println("Calories dans assiette 2 : " + assiette2.nbCalories);
         System.out.println("Calories dans assiette 3 : " + assiette3.nbCalories);
         
+        
+        
+        
+
+        }
     }
-    
-}
